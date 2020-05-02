106043261ef2b7b8a045ec63d8788c250849f78a
Thoa Duong <kimthoa2598@gmail.com>
2020-05-02 14:56:33 +0700

    Updates

---
+++ precache-manifest.70b5d2e1350d5d595cd807effb721211.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2f4e85e4bd03f14a085aac98cee406",
+    "url": "/simple-sotas/index.html"
+  },
+  {
+    "revision": "3356d5d648d55212dd09",
+    "url": "/simple-sotas/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b43e6319d564acc65d5b",
+    "url": "/simple-sotas/static/css/main.656e6314.chunk.css"
+  },
+  {
+    "revision": "3356d5d648d55212dd09",
+    "url": "/simple-sotas/static/js/2.2b3e367c.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/simple-sotas/static/js/2.2b3e367c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43e6319d564acc65d5b",
+    "url": "/simple-sotas/static/js/main.34fbe336.chunk.js"
+  },
+  {
+    "revision": "3c5a9ce0061156ac4f9d",
+    "url": "/simple-sotas/static/js/runtime-main.9f3151ec.js"
+  },
+  {
+    "revision": "9774311f98483e2020dc035258c8fa49",
+    "url": "/simple-sotas/static/media/bg-01.9774311f.jpg"
+  }
+]);
